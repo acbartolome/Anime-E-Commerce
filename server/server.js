e6d4bd72425497ed49b1b33125ad2aa2b3a1ca04
adryanbartolome <adryanbartolome8123@gmail.com>
2024-07-11 18:59:22 -0700

    Added cors to server.js

--- server/server.js
+++ server/server.js
@@ -3,8 +3,10 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+// add cors
+const cors = require("cors");
+app.use(cors);
 // add more routes here?
-
 // iniialize server
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
