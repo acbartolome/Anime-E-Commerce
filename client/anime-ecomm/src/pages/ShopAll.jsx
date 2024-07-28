ee068779a5f2cce66c96dc4f38d5150f56a121ac
adryanbartolome <adryanbartolome8123@gmail.com>
2024-07-28 13:02:48 -0700

    Here is the start of the shop all page

--- client/anime-ecomm/src/pages/ShopAll.jsx
+++ client/anime-ecomm/src/pages/ShopAll.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
 const ShopAll =
   (/* add functionality here for loggining{ isLoggedIn, setCart }*/) => {
     // establishng the potential useState
@@ -12,13 +15,26 @@ const ShopAll =
     useEffect(() => {
       const fetchProducts = async () => {
         try {
-          const response = await fetch();
-          // "https://localhost:3000/"
-        } catch (error) {}
+          const response = await fetch("https://localhost:3000/server/product");
+          const data = await response.json();
+          setProducts(data.products);
+        } catch (error) {
+          console.error(error);
+        }
       };
-    });
+      fetchProducts();
+    }, []);
 
-    return <div>SHOP ALL PAGE</div>;
+    return (
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Title>LIMITED EDITION</Card.Title>
+          <Card.Text>$4,999.99</Card.Text>
+          <Button variant="primary">Add to cart</Button>
+        </Card.Body>
+      </Card>
+    );
   };
 
 export default ShopAll;
