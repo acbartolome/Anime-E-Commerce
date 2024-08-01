6ff02ed363a94877cd44e7212c4ea5a5717420de
adryanbartolome <adryanbartolome8123@gmail.com>
2024-07-31 18:28:41 -0700

    Started teh Login.jsx page

--- client/anime-ecomm/src/pages/Login.jsx
+++ client/anime-ecomm/src/pages/Login.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const Login = () => {
-  return <div>LOGIN PAGE</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  // will add login functionality after API route is created
+  const handleLogin = async (event) => {};
+
+  return (
+    <>
+      <FloatingLabel
+        controlId="floatingInput"
+        label="Email address"
+        className="mb-3"
+      >
+        <Form.Control type="email" placeholder="name@example.com" />
+      </FloatingLabel>
+      <FloatingLabel controlId="floatingPassword" label="Password">
+        <Form.Control type="password" placeholder="Password" />
+      </FloatingLabel>
+      <Button variant="dark">Login</Button>
+    </>
+  );
 };
 
 export default Login;
