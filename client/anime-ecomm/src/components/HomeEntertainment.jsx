8b51d17c34bad50fb069ad20288f6807ef998379
Bradley Chery <bchery618@gmail.com>
2024-08-01 01:20:06 -0400

    fix later

---
+++ client/anime-ecomm/src/components/HomeEntertainment.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomeEntertainment = () => {
+  return <div>HOME ENTERTAINMENT PAGE</div>;
+};
+
+export default HomeEntertainment;
---
+++ client/anime-ecomm/src/components/OrderHistory.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OrderHistory = () => {
+  return <div>ORDER HISTORY PAGE</div>;
+};
+
+export default OrderHistory;
