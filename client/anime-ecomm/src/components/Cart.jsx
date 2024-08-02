31ee56e53559107745792fce0ed8390c77163627
Bradley Chery <bchery618@gmail.com>
2024-08-01 20:36:46 -0400

    deleted files in components

--- client/anime-ecomm/src/components/Cart.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Cart = () => {
-  return <div>CART PAGE</div>;
-};
-
-export default Cart;
--- client/anime-ecomm/src/components/SingleProduct.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SingleProduct = () => {
-  return <div>SINGLE PRODUCT PAGE</div>;
-};
-
-export default SingleProduct;
